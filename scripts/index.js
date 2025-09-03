329fddac09beb5d31cba311e2058efc4d9d71cdb
Pratush <pratushbarua26@gmail.com>
2025-09-04 00:18:42 +0600

    empty handling

--- scripts/index.js
+++ scripts/index.js
@@ -7,18 +7,28 @@ let level = (l) => {
   fetch(`${levelBaseUrl}${l}`)
     .then(res => res.json())
     .then(json => {
-      json.data.map(e => {
+      container.innerHTML = "";
+      if (json.data.length == 0) {
         let box = document.createElement("span");
-        box.className = "flex flex-col gap-3 items-center justify-center bg-white p-6 rounded-lg";
-        box.innerHTML = `<h5 class="text-2xl font-bold">${e.word}</h5>
-                    <p class="text-sm bangla">Pronounciation - ${e.pronunciation}</p>
-                    <h6 class="text-lg bangla font-semibold">Meaning - ${e.meaning}</h6>
-                    <div class="flex items-center justify-between w-full">
-                        <i class="fa-solid fa-circle-info"></i>
-                        <i class="fa-solid fa-volume-high"></i>
-                    </div>`;
+        box.className = "flex flex-col gap-3 items-center justify-center bangla col-span-3 p-6 rounded-lg";
+        box.innerHTML = `<img src="./assets/alert-error.png" alt="alert sign">
+                      <small class="text-gray-500 text-xs font-semibold">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</small>
+                    <p class="font-semibold text-lg text-gray-800">নেক্সট Lesson এ যান</p>`
         container.append(box);
-      });
+      } else {
+        json.data.map(e => {
+          let box = document.createElement("span");
+          box.className = "flex flex-col gap-3 items-center justify-center bg-white p-6 rounded-lg";
+          box.innerHTML = `<h5 class="text-2xl font-bold">${e.word}</h5>
+                      <p class="text-sm bangla">Pronounciation - ${e.pronunciation}</p>
+                      <h6 class="text-lg bangla font-semibold">Meaning - ${e.meaning}</h6>
+                      <div class="flex items-center justify-between w-full">
+                          <i class="fa-solid fa-circle-info"></i>
+                          <i class="fa-solid fa-volume-high"></i>
+                      </div>`;
+          container.append(box);
+        });
+      }
     });
 }
 
