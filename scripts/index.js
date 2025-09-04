76b911785ea428a5d0a275b805f8b2a92f0d652a
Pratush <pratushbarua26@gmail.com>
2025-09-04 12:20:27 +0600

    word details functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,29 @@
+//  some utilities
 let lessonBar = document.getElementById("lesson-bar");
-let AllLevelUrl = "https://openapi.programming-hero.com/api/levels/all";
-let levelBaseUrl = "https://openapi.programming-hero.com/api/level/";
 let container = document.getElementById("container");
 
+//  {word: 'Fascinate', meaning: 'মুগ্ধ করা', pronunciation: 'ফ্যাসিনেট', level: 5, sentence: "The magician's tricks fascinate the audience.", …}
+//  word info button's functionality
+let wordDetails = async (id) => {
+  let details = await (await fetch(`https://openapi.programming-hero.com/api/word/${id}`)).json();
+  document.querySelector("#my_modal_5 h3").innerHTML = `${details.data.word ? details.data.word : "not found"} (<i class="fa-solid fa-microphone-lines"></i> : ${details.data.pronunciation ? details.data.pronunciation : "not found"})`
+  document.querySelector("#my_modal_5 .meaning").innerHTML = details.data.meaning ? details.data.meaning : "not found";
+  document.querySelector("#my_modal_5 .example").innerHTML = details.data.sentence ? details.data.sentence : "not found";
+  details.data.synonyms.forEach(e => {
+    console.log(e)
+    let b = document.createElement("span")
+    b.className = "bg-sky-100 text-sm px-4 py-2 rounded"
+    b.innerText = e
+    document.querySelector("#my_modal_5 .synonyms").appendChild(b)
+  })
+  console.log(details.data)
+  my_modal_5.showModal();
+}
+
+
+//  level button functionality
 let level = (l) => {
-  fetch(`${levelBaseUrl}${l}`)
+  fetch(`https://openapi.programming-hero.com/api/level/${l}`)
     .then(res => res.json())
     .then(json => {
       container.innerHTML = "";
@@ -18,29 +37,36 @@ let level = (l) => {
       } else {
         json.data.map(e => {
           let box = document.createElement("span");
-          box.className = "flex flex-col gap-3 items-center justify-center bg-white p-6 rounded-lg";
-          box.innerHTML = `<h5 class="text-2xl font-bold">${e.word}</h5>
-                      <p class="text-sm bangla">Pronounciation - ${e.pronunciation}</p>
-                      <h6 class="text-lg bangla font-semibold">Meaning - ${e.meaning}</h6>
-                      <div class="flex items-center justify-between w-full">
-                          <i class="fa-solid fa-circle-info"></i>
-                          <i class="fa-solid fa-volume-high"></i>
-                      </div>`;
-          container.append(box);
+          box.className = "flex flex-col gap-3 items-center justify-center text-center bg-white p-6 rounded-lg";
+          box.innerHTML = `<h5 class="text-2xl font-bold">${e.word ? e.word : "word not found"}</h5>
+                    <p class="text-sm bangla">Pronounciation - ${e.pronunciation ? e.pronunciation : "উচ্চারণ পাওয়া যায়নি"}</p>
+                    <h6 class="text-lg bangla font-semibold">Meaning - ${e.meaning ? e.meaning : "শব্দার্থ পাওয়া যায়নি"}</h6>
+                    <div class="flex items-center justify-between w-full">
+                        <button class="btn" onclick="wordDetails(${e.id})"><i class="fa-solid fa-circle-info"></i></button>
+                        <button class="btn" onclick="wordDetails(${e.id})"><i class="fa-solid fa-volume-high"></i></button>
+                    </div>`;
+          container.appendChild(box);
         });
       }
     });
 }
 
-// {id: 3, level: 2, word: 'Cautious', meaning: 'সতর্ক', pronunciation: 'কশাস'}
-fetch(AllLevelUrl)
+
+//  Show all levels in the UI with active buttons
+fetch("https://openapi.programming-hero.com/api/levels/all")
   .then(res => res.json())
   .then(json => {
     json.data.map(e => {
       let btn = document.createElement("button");
       btn.className = "btn btn-outline btn-primary text-xs p-2 h-fit";
       btn.innerHTML = `<i class="fa-solid fa-book-open"></i> Lesson - ${e.level_no}`;
-      btn.addEventListener("click", () => level(e.level_no));
-      lessonBar.append(btn);
+      btn.addEventListener("click", (event) => {
+        lessonBar.querySelectorAll("button").forEach(b => {
+          b.classList.remove("active");
+        });
+        event.target.classList.add("active");
+        level(e.level_no)
+      });
+      lessonBar.appendChild(btn);
     });
   });
